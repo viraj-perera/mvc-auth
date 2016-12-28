1fd3f36d85ddd5525fc275c83f34296900ba4bd8
Viraj Perera <vipe@mazarin.lk>
2016-12-28 10:31:37 +0530

    Updated the test case.

--- src/test/java/vipe/test/repository/UserRepositoryTest.java
+++ src/test/java/vipe/test/repository/UserRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.util.Assert;
 import vipe.test.config.TestAppConfig;
+import vipe.test.model.User;
 
 /**
  * Created by viraj on 12/27/16.
@@ -23,6 +24,11 @@ public class UserRepositoryTest {
   @Test
   public void sampleTest() {
     Assert.notNull(userRepository);
+    User user = userRepository.findBySsoId("none_existent");
+    Assert.isNull(user);
+    //We have a user with the user name "vipe"
+    user = userRepository.findBySsoId("vipe");
+    Assert.isTrue(user.getSsoId().equalsIgnoreCase("vipe"));
     Assert.notNull(new Object());
   }
 }
